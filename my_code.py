06409a0a6dacd8cfddf33e53dd45560062c77f13
keeliescholhamer <24kscholhamer@athenian.org>
2020-10-27 09:25:28 -0700

    D Period

--- my_code.py
+++ my_code.py
@@ -1,3 +1,7 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
-
+print('Hello world')
+name='Keelie'
+print(f'Hello {name}')
+name2 = 'Friends'
+print(f' Welcome {name} and {name2}')
