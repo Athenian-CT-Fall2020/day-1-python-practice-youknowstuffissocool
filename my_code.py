bcd19236af25d9b79f43d7566033c9a4b4d1d3b8
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-10-27 08:43:40 -0700

    Initial commit

---
+++ my_code.py
@@ -0,0 +1,3 @@
+# This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
+# Collaborators:               (fill this is on every assignment, even if the answer is "none")
+
